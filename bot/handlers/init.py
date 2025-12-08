958a02bc1328039b5408159940ef81c240b5fc31
veedva <chauhochesh@gmail.com>
2025-12-08 05:42:43 +0300

    Update init.py

--- bot/handlers/init.py
+++ bot/handlers/init.py
@@ -1,10 +1,13 @@
 from aiogram import Router
+
 from .start import router as start_router
 from .hold import router as hold_router
 from .progress import router as progress_router
-# сюда потом добавим остальные
+# сюда потом добавим rage, achievements и т.д.
 
 router = Router()
 router.include_router(start_router)
 router.include_router(hold_router)
 router.include_router(progress_router)
+
+__all__ = ["router"]
