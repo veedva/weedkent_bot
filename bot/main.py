1de101c466168153f9e68131910e4e6c0c90aa72
veedva <chauhochesh@gmail.com>
2025-12-08 06:56:46 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,7 @@
 import asyncio
 import logging
+import subprocess
+
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
@@ -7,7 +9,6 @@ from aiogram.enums import ParseMode
 
 from bot.config import BOT_TOKEN
 from bot.handlers import router
-from bot.db import create_tables  # ← важно
 
 bot = Bot(
     token=BOT_TOKEN,
@@ -25,8 +26,9 @@ async def main():
     await on_startup()
     await dp.start_polling(bot)
 
-
-# 👇 временный запуск
 if __name__ == "__main__":
-    asyncio.run(create_tables())  # ← создает таблицы
-    asyncio.run(main())           # ← запускает бота
+    # 🟢 ВРЕМЕННЫЙ вызов миграции
+    subprocess.run(["python", "create_tables.py"])
+
+    # ▶️ Старт бота
+    asyncio.run(main())
