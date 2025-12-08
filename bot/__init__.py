08690cc8b7064190eb19cbd48a0121f5a07d6d11
veedva <chauhochesh@gmail.com>
2025-12-08 05:10:43 +0300

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,10 +1,1 @@
-from aiogram import Router
-from .start import router as start_router
-from .hold import router as hold_router
-from .progress import router as progress_router
-# сюда потом добавим остальные
 
-router = Router()
-router.include_router(start_router)
-router.include_router(hold_router)
-router.include_router(progress_router)
