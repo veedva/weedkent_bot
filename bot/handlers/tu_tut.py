b542dbd4c1df92c69a3adb1447f7ca376d7fc5e5
veedva <chauhochesh@gmail.com>
2025-12-09 09:40:35 +0300

    Update tu_tut.py

--- bot/handlers/tu_tut.py
+++ bot/handlers/tu_tut.py
@@ -5,11 +5,12 @@ import random
 
 router = Router()
 
-TU_TUT = ["Тут.", "Привет.", "Ага.", "Здесь.", "Да, брат."]
+TU_TUT_FIRST = ["Тут.", "Привет.", "А куда я денусь?", "Здесь.", "Да, брат."]
+TU_TUT_SECOND = ["Держись, я рядом.", "Я с тобой.", "Всё будет.", "Не сдавайся ✊"]
 
 @router.message(F.text == "👋 Ты тут?")
 async def tu_tut(message: Message):
+    await asyncio.sleep(random.randint(2, 6))
+    await message.answer(random.choice(TU_TUT_FIRST))
     await asyncio.sleep(random.randint(2, 5))
-    await message.answer(random.choice(TU_TUT))
-    await asyncio.sleep(random.randint(2, 4))
-    await message.answer("Держись ✊")
+    await message.answer(random.choice(TU_TUT_SECOND))
