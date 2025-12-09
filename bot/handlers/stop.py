82e711b48b7f836aa0556f236eb2fa5856db25a3
veedva <chauhochesh@gmail.com>
2025-12-09 09:41:38 +0300

    Update stop.py

--- bot/handlers/stop.py
+++ bot/handlers/stop.py
@@ -5,8 +5,8 @@ from bot.utils.user import get_user, save_user
 router = Router()
 
 @router.message(F.text == "⏸ Помолчи")
-async def stop(message: Message):
+async def stop_bot(message: Message):
     user = await get_user(message.from_user.id)
     user.active = False
     await save_user(user)
-    await message.answer("Рассылки остановлены. Возвращайся, когда готов ✊")
+    await message.answer("Рассылки остановлены.\nВозвращайся, когда будешь готов ✊")
