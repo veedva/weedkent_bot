a8f06b998fc09f4cfce1627ab6f946f385a88e15
veedva <chauhochesh@gmail.com>
2025-12-09 07:50:39 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,5 @@
 import asyncio
 import logging
-import subprocess
-
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
@@ -9,6 +7,9 @@ from aiogram.enums import ParseMode
 
 from bot.config import BOT_TOKEN
 from bot.handlers import router
+from bot.jobs import start_scheduler
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 bot = Bot(
     token=BOT_TOKEN,
@@ -18,17 +19,13 @@ dp = Dispatcher(storage=MemoryStorage())
 dp.include_router(router)
 
 async def on_startup():
-    logging.basicConfig(level=logging.INFO)
     print("@weedkent_bot — онлайн и готов держать с тобой до конца ✊")
     print("WeedKent Pro 2025 — активирован")
+    await start_scheduler(bot)  # запускаем рассылки
 
 async def main():
     await on_startup()
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    # 🟢 ВРЕМЕННЫЙ вызов миграции
-    subprocess.run(["python", "create_tables.py"])
-
-    # ▶️ Старт бота
     asyncio.run(main())
