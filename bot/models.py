bfd10cb7a256e313f02b031172ccb9d7c9901ba7
veedva <chauhochesh@gmail.com>
2025-12-09 07:52:39 +0300

    Update models.py

--- bot/models.py
+++ bot/models.py
@@ -1,7 +1,6 @@
-from datetime import date
-from sqlalchemy import Column, BigInteger, Date, Boolean, Integer, JSON, String, DateTime
+from sqlalchemy import Column, Integer, Boolean, Date, DateTime, JSON, BigInteger
 from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime
+from datetime import date, datetime
 
 Base = declarative_base()
 
@@ -9,12 +8,11 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(BigInteger, primary_key=True)
-    start_date = Column(Date, nullable=True)
     active = Column(Boolean, default=False)
+    start_date = Column(Date, nullable=True)
     best_streak = Column(Integer, default=0)
     hold_count_today = Column(Integer, default=0)
     last_hold_date = Column(Date, nullable=True)
     last_hold_time = Column(DateTime, nullable=True)
-    achievements = Column(JSON, default=list)  # список полученных достижений
-    mood_history = Column(JSON, default=list)  # [(date, mood_1_10), ...]
-    # остальные поля можно добавить позже
+    achievements = Column(JSON, default=list)
+    mood_history = Column(JSON, default=list)  # [(date, mood_1_10)]
