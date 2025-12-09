d1b0119a78ce633e4b7fa503bf66ff0304bb8a3d
veedva <chauhochesh@gmail.com>
2025-12-09 07:52:17 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,7 +5,10 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN не найден в переменных окружения!")
+    raise ValueError("BOT_TOKEN не найден!")
 
-POSTGRES_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL не найден! Добавь в Railway")
+
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
